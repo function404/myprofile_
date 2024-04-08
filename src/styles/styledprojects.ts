3da75cd1bc51c531622e8580d5a90c92e8584b40
lincoln <lincolnnovais15a@gmail.com>
2024-04-08 09:39:24 -0300

    feat: new animation for text

--- src/styles/styledprojects.ts
+++ src/styles/styledprojects.ts
@@ -3,15 +3,69 @@ import { motion } from 'framer-motion'
 import styled, { keyframes } from 'styled-components'
 import { colors } from '^/app/utils/colors'
 
-const title = keyframes`
-   0%,
-   10%,
-   100% {
-      width: 0;
+const glitchAnimation = keyframes`
+   0% {
+      clip: rect(18px, 9999px, 82px, 0);
    }
-   70%,
+   5% {
+      clip: rect(36px, 9999px, 64px, 0);
+   }
+   10% {
+      clip: rect(85px, 9999px, 12px, 0);
+   }
+   15% {
+      clip: rect(2px, 9999px, 89px, 0);
+   }
+   20% {
+      clip: rect(58px, 9999px, 51px, 0);
+   }
+   25% {
+      clip: rect(94px, 9999px, 66px, 0);
+   }
+   30% {
+      clip: rect(9px, 9999px, 86px, 0);
+   }
+   35% {
+      clip: rect(75px, 9999px, 44px, 0);
+   }
+   40% {
+      clip: rect(53px, 9999px, 84px, 0);
+   }
+   45% {
+      clip: rect(64px, 9999px, 92px, 0);
+   }
+   50% {
+      clip: rect(70px, 9999px, 56px, 0);
+   }
+   55% {
+      clip: rect(6px, 9999px, 27px, 0);
+   }
+   60% {
+      clip: rect(68px, 9999px, 30px, 0);
+   }
+   65% {
+      clip: rect(93px, 9999px, 41px, 0);
+   }
+   70% {
+      clip: rect(16px, 9999px, 4px, 0);
+   }
+   75% {
+      clip: rect(17px, 9999px, 24px, 0);
+   }
+   80% {
+      clip: rect(96px, 9999px, 18px, 0);
+   }
+   85% {
+      clip: rect(97px, 9999px, 9px, 0);
+   }
    90% {
-      width: 100%;
+      clip: rect(25px, 9999px, 95px, 0);
+   }
+   95% {
+      clip: rect(8px, 9999px, 23px, 0);
+   }
+   100% {
+      clip: rect(94px, 9999px, 38px, 0);
    }
 `
 export const StyledTitle = styled.div`
@@ -27,22 +81,30 @@ export const StyledTitleH2 = styled.h2`
    font-size: 72px;
    font-family: "Poppins", sans-serif;
    margin-bottom: 80px;
-   -webkit-text-stroke: 0.1vw ${colors.colorPrimaryImg};
+   color: ${colors.colorPrimaryImg};
    text-transform: uppercase;
    letter-spacing: 5px;
 
-   &::before {
+   &::before,
+   &::after {
       content: attr(data-text);
       position: absolute;
       top: 0;
       left: 0;
-      width: 0;
+      width: 100%;
       height: 100%;
-      color: ${colors.colorPrimaryImg};
-      -webkit-text-stroke: 0vw;
-      border-right: 2px solid ${colors.colorPrimary};
-      overflow: hidden;
-      animation: ${title} 6s linear infinite;
+   }
+
+   &::before {
+      left: 2.5px;
+      text-shadow: -2.5px 0 ${colors.colorWhite};
+      animation: ${glitchAnimation} 7s infinite linear alternate-reverse;
+   }
+   
+   &::after {
+      left: -2.5px;
+      text-shadow: 2.5px 0 ${colors.colorBlack};
+      animation: ${glitchAnimation} 8s infinite linear alternate-reverse;
    }
 
    @media screen and (max-width: 960px) {
